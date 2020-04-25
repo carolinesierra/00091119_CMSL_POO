66671b26dcce1c6ffde7ec7b8ee4545deef18632
Caroline Sierra <00091119@uca.edu.sv>
2020-04-25 00:40:11 -0600

    exercise 2 has been added

---
+++ Labo 05/Ejercicio02/Ejercicio02/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+namespace Ejercicio02{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Digite la frase que desea verificar(Por favor omita mayusculas y tildes):  ");
+            string frase = Console.ReadLine();
+            
+            
+            if (FunPalindroma(frase)) {
+                Console.WriteLine("Siuuuuuuuuuu, la frase es palindroma");
+            } else {
+                Console.WriteLine("La frase no es plaindroma:(");
+            }
+        }
+        static bool FunPalindroma(String frase)
+        {
+            string b = ""; 
+            int n = frase.Length;
+
+            int i;
+            for (i = 0; i < n; ++i){
+                if (!(frase.Substring(i,  1).Equals(" ")
+                    || frase.Substring(i, 1).Equals(",")
+                    || frase.Substring(i,  1).Equals("?")
+                    || frase.Substring(i,  1).Equals(".")
+                    || frase.Substring(i,  1).Equals("´")
+                    || frase.Substring(i,  1).Equals("!")
+                    || frase.Substring(i,  1).Equals("¡")
+                    || frase.Substring(i,  1).Equals(";")
+                    || frase.Substring(i,  1).Equals("¿")))
+                {
+                    b += frase.Substring(i,  1);
+                }
+            }
+
+            n = b.Length;
+            int aux = n;
+
+            for(i = 0; i < n / 2; ++i) {
+                if (!b.Substring(i, 1).Equals(b.Substring(aux - 1, 1))) {
+                    return false;
+                }
+                aux--;
+            }
+            return true;
+        }
+        
+    }
+}
