918bff3255df10488ee3e0dd01621fc8450cb474
Caroline Sierra <00091119@uca.edu.sv>
2020-04-25 00:32:12 -0600

    exercise 1 has been added

---
+++ Labo 05/Ejercicio01/Ejercicio01/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace Ejercicio01
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int a = 1, b = 0, c = 1;
+
+            Console.WriteLine("Digite la cantidad de números que desea de la serie Fibonacci: ");
+            int numero = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("----Mostrando serie----");
+
+            Console.WriteLine(a + " ");
+
+            for (var i = 1; i < numero; i++)
+            {
+                a = b + c;
+                Console.WriteLine(a + " ");
+                b = c;
+                c = a;
+            }
+        }
+    }
+}
