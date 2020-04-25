724ba09427e4a61969db31c501e32df9755d558f
Caroline Sierra <00091119@uca.edu.sv>
2020-04-25 00:46:46 -0600

    exercise 3 has been added

---
+++ Labo 05/Ejercicio03/Ejercicio03/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using static System.Convert;
+
+namespace Ejercicio03
+{
+    internal class Program
+    {
+        private static int _numero;
+        public static void Main(string[] args){
+
+            Console.WriteLine("Sabiendo esto, por favor ingrese un número:  ");
+            _numero = ToInt32(Console.ReadLine());
+            
+            Console.WriteLine("Su lista es:  ");
+            Secuencia(1);
+            
+        }
+        static void Secuencia(int a) {
+            Console.WriteLine( a + " ");
+            
+            if (a != _numero)
+            {
+                Secuencia(a +1);
+                Console.WriteLine( a + " ");
+            }
+
+        }
+    }
+}
