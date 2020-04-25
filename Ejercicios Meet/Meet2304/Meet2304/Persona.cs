8f0edb35fdb30be9085c302456bbb0c9edab9686
Caroline Sierra <00091119@uca.edu.sv>
2020-04-25 01:09:02 -0600

    added exercise

---
+++ Ejercicios Meet/Meet2304/Meet2304/Persona.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace Meet2304
+{
+    public class Persona
+    {
+        private String nombre, apellido;
+        private int edad;
+
+        public string Nombre
+        {
+            get => nombre;
+            set => nombre = value;
+        }
+        
+        public Persona(String nombre = "sin nombre",
+            String apellido = "sin apellido", 
+            int edad = 0)
+        {
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.edad = edad;
+
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(nombre)}: {nombre}, {nameof(apellido)}: {apellido}," +
+                   $" {nameof(edad)}: {edad}";
+        }
+
+        public String saludar(String saludoP)
+        {
+            return nombre + " " + saludoP;
+        }
+
+    }
+}
---
+++ Ejercicios Meet/Meet2304/Meet2304/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Meet2304
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            //List<Persona> p = new List<Persona>();
+            var p = new List<Persona>();
+            
+            p.Add(new Persona(nombre: "Caroline", apellido: "Papitas", edad: 20));
+            p.Add(new Persona(nombre: "TengoHambre", edad: 45));
+            p.Add(new Persona(edad: 19));
+            p.Add(new Persona(apellido: "YTengoSed"));
+
+            String buscar = "Caroline";
+            buscar = Console.ReadLine();
+            
+            p.ForEach(it =>
+            {
+                if (buscar.Equals(it.Nombre))
+                {
+                    Console.WriteLine("Nuevo nombre: ");
+                    it.Nombre = Console.ReadLine();
+                    Console.WriteLine(it.ToString());
+                }
+            });
+            
+            int añadir = 12;
+            int árbol = 14;
+        }
+    }
+}
