e7d9d24d8e3ae9ea1c096017a6aa1680a857edb2
Caroline Sierra <00091119@uca.edu.sv>
2020-04-25 00:56:39 -0600

    exercise 4 has been added

---
+++ Labo 05/Ejercicio04/Ejercicio04/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace Ejercicio04
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Ingrese una cadena de texto: ");
+            String cadena = Console.ReadLine();;
+
+
+            Console.WriteLine("El programa cambio las letras 'a' por la letra 'b' de su cadena ingresada.");
+            Console.WriteLine("Su nueva cadena es:D  :");
+
+            String cambio = cadena.Replace('a','b'); //Metodo para cambiar caracteres. oldChar:Caracter a sustituir y newChar:caracter que reemplaza al viejo caracter.
+            Console.WriteLine(cambio);
+        }
+    }
+}
+
